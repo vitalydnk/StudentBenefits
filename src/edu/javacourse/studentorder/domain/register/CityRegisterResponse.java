e97ccbc26dc11f96769b519a258d30b7d4f2d8be
vital <vitalydnk@gmail.com>
2022-01-09 13:36:58 +0300

    Database and new entities created

--- src/edu/javacourse/studentorder/domain/register/CityRegisterResponse.java
+++ src/edu/javacourse/studentorder/domain/register/CityRegisterResponse.java
@@ -1,4 +1,4 @@
-package edu.javacourse.studentorder.domain;
+package edu.javacourse.studentorder.domain.register;
 
 public class CityRegisterCheckerResponse {
     private boolean existing;
@@ -18,5 +18,13 @@ public class CityRegisterCheckerResponse {
 
     public void setTemporal(Boolean temporal) {
         this.temporal = temporal;
+    }
+
+    @Override
+    public String toString() {
+        return "CityRegisterCheckerResponse{" +
+                "existing=" + existing +
+                ", temporal=" + temporal +
+                '}';
     }
 }
---
+++ src/edu/javacourse/studentorder/exception/TransportException.java
@@ -0,0 +1,6 @@
+package edu.javacourse.studentorder.exception;
+
+public class TransportException extends Throwable {
+    public TransportException(String s) {
+    }
+}
